496ffd1cae4d6facd02dd2715182cc34c1727964
baisichen <baisichen@baidu.com>
2021-05-20 21:18:51 +0800

    词法分析-浮点数识别

---
+++ 练手小程序/词法分析/浮点数识别/README.md
@@ -0,0 +1,102 @@
+<!--
+ * @Author: baisichen
+ * @Date: 2021-05-19 20:41:10
+ * @LastEditTime: 2021-05-20 19:46:33
+ * @LastEditors: baisichen
+ * @Description: 
+-->
+# 词法分析-浮点数识别
+> 参考：http://www.cppblog.com/woaidongmao/archive/2009/09/25/97245.aspx
+# 总体设计
+
+## 输入
+屏幕输入或文本或程序中的字符串作为输入
+### 输出
+识别出的一个浮点数字串
+### 测试集
+可以使用https://leetcode-cn.com/problems/biao-shi-shu-zhi-de-zi-fu-chuan-lcof
+
+### 一个浮点数的定义：
+- 符号部分，+或-
+- 整数部分，由数字串组成
+- 小数部分，以字符'.'为开始后跟数字串
+- 指数部分，由E或e开头后跟带符号的数字串(这里暂不考虑指数为小数的情况)
+
+### 确定有限自动机
+因为场景比较简单，这里直接给出确定有限自动机. 这里面并不是严格意义上的确定有限自动机，其中+/-和e/E都是跳转到同一个状态，为了书写方便才这样写的，在状态转移表中需要拆开。
+<div align=center>
+<img src="https://github.com/ZYBaisichen/MarkdownImages/blob/main/float_digit_finite_automata.jpeg" />
+</div>
+
+### 状态表
+|状态|输入|下一个状态|
+|---|---|---|---|
+|0|开始状态||
+|0|+|1|
+|0|-|1|
+|0|digit|2|
+|1|digit|2|
+|2|digit|2|
+|2|.|3|
+|2|e|5|
+|2|E|5|
+|2|other|8|
+|3|digit|4|
+|3|other|9|
+|4|digit|4|
+|4|e|5|
+|4|E|5|
+|4|other|8|
+|5|+|6|
+|5|-|6|
+|5|digit|7|
+|5|other|9|
+|6|digit|7|
+|6|other|9|
+|7|digit|7|
+|7|other|8|
+|8|结束状态||
+|9|错误状态||
+
+### 详细设计
+#### FoatPoint
+类名，识别浮点数.
+- 成员变量
+  - _f： 映射关系表
+  - _state: 当前状态
+  - _curToken: 当前识别的结果
+  - _input_str: 输入的字符串
+- 成员函数
+  - getNextToken
+  - isDigit
+  - getChar
+  - error_log
+  - getCurToken
+
+#### _f
+状态转移关系表，_f[state][ch]中存储在state状态下输入ch之后的下一个状态号nextstate
+
+#### _state 
+记录当前状态，每次getChar时根据状态转移关系表改变状态
+
+#### _curToken
+当前已经识别为合法的数字串，当两个结束状态时清空
+
+#### _input_str
+要解析的输入字符串，初始化时指定
+
+#### getNextToken()
+主体程序，获得下一个浮点数
+
+#### isDigit()
+识别是否为数字的函数, 判断当前输入的字符是否在0-9之间
+
+#### getChar()
+获取下一个字符
+
+#### error_log()
+错误处理函数，处理状态9的报错，并打印日志
+
+#### getCurToken()
+拿到当前的识别出的数字字符串
+
---
+++ 练手小程序/词法分析/浮点数识别/get_float.hpp
@@ -0,0 +1,261 @@
+/***
+ * @Author: baisichen
+ * @Date: 2021-05-19 20:17:45
+ * @LastEditTime: 2021-05-20 21:17:15
+ * @LastEditors: baisichen
+ * @Description: 词法分析-识别浮点数
+ */
+#include<iostream>
+#include<vector>
+#include<map>
+#include<string>
+#include<set>
+using namespace std;
+class FloatPoint{
+public:
+	// @breif:      初始化浮点数分析对象构造函数
+	// @param[in]:  [string&]input 需要识别的代码段
+	// @return:
+	FloatPoint(std::string& input) {
+		_input_str = input;
+		init();
+	}
+
+	// @breif:      初始化文法、初始化状态
+	// @param[in]:
+	// @return:     是否成功 0:成功 -1失败
+	int init();
+
+	// @breif:      获取下一个浮点数字
+	// @param[out]:  [string&] 识别出来的浮点数字串
+	// @return:     是否成功 0:成功 -1失败
+	int getNextToken(std::string& token);
+
+	// @breif:      判断字符是否为数字
+	// @param[in]:  [char] 单个字符
+	// @return:     true:为数字 false:不为数字
+	bool isDigit(char ch);
+
+	// @breif:      输出错误信息
+	// @param[out]:  [std:;string&] 错误信息
+	// @return:     是否成功 0:成功 -1失败
+	int error_log(std::string& mess);
+
+
+	// @breif:      拿到当前已经识别出来的浮点数串
+	// @param[out]:  [std::string&] 当前串
+	// @return:     是否成功 0:成功 -1失败
+	int getCurToken(std::string& curToken);
+
+	// @breif:      判断是否为数字、e/E、+/-、空格之外的字符
+	// @param[in]:  [char] ch
+	// @return:     是否成功 0:成功 -1失败
+	bool isOther(char ch);
+
+	// @breif:      获取下一个字符，先将字符指针加一，如果识别到了最后一个字符之后，则返回-1
+	// @param[out]:  [char*] ch
+	// @return:     是否成功 0:成功 -1失败
+	int getChar(char* ch);
+
+	// @breif:     恢复自动机状态
+	// @param[out]:  [char*] ch
+	// @return:     是否成功 0:成功 -1失败
+	int restoreAutonmata();
+
+private:
+	//状态转移关系；确定有限自动机转换图
+	map<int, map<char, int> > _f;
+	//当前状态
+	int _state;
+	//当前识别的结果
+	std::string _curToken;
+	//输入字符串
+	std::string _input_str;
+	//在识别串中的位置
+	int ptr;
+	//字符集
+	set<char> all_char_set;
+};
+
+// @breif:      初始化文法、初始化状态
+int FloatPoint::init() {
+	int res = 0;
+	//初始化文法
+	_f[0]['+'] = 1;
+	_f[0]['-'] = 1;
+	_f[0]['d'] = 2;
+	_f[1]['d'] = 2;
+	_f[2]['d'] = 2;
+	_f[2]['.'] = 3;
+	_f[2]['e'] = 5;
+	_f[2]['o'] = 8;
+	_f[3]['d'] = 4;
+	_f[3]['o'] = 9;
+	_f[4]['d'] = 4;
+	_f[4]['e'] = 5;
+	_f[4]['E'] = 5;
+	_f[4]['o'] = 8;
+	_f[5]['+'] = 6;
+	_f[5]['-'] = 6;
+	_f[5]['d'] = 7;
+	_f[5]['o'] = 9;
+	_f[6]['o'] = 9;
+	_f[6]['d'] = 7;
+	_f[7]['d'] = 7;
+	_f[7]['o'] = 8;
+
+	//初始化当前状态
+	_state = 0;
+
+	//初始化已经识别的字符串
+	_curToken = "";
+
+	//在识别串中的位置，初始为-1
+	ptr = -1;
+
+	all_char_set.insert('e');
+	all_char_set.insert('E');
+	all_char_set.insert('+');
+	all_char_set.insert('-');
+	all_char_set.insert(' ');
+	all_char_set.insert('.');
+	return res;
+}
+
+// @breif:      获取下一个浮点数字
+int FloatPoint::getNextToken(std::string& token) {
+	int res = 0;
+	int _input_len = _input_str.length();
+	while (ptr < _input_len) {
+		if (_state == 8) {
+			res = getCurToken(token);
+			restoreAutonmata();//恢复自动机为初始状态
+			break;
+		} else if(_state == 9) {
+			res = error_log(token);
+			restoreAutonmata();//恢复自动机为初始状态
+			break;
+		}
+
+		char ch = '\0';
+		res = getChar(&ch);
+		if (res == -1) {
+			getCurToken(token);
+			restoreAutonmata();//恢复自动机为初始状态
+			break;
+		}
+
+		//开始状态特殊处理， 非数字、加减号进入不了状态机
+		if(_state == 0 && !isDigit(ch) && ch!='+' && ch!='-') {
+			continue;
+		}
+
+		//获取下一个状态
+		if (isDigit(ch)) {
+			//数字，文法中没定义转移，则报错
+			map<char, int>::iterator iter = _f[_state].find('d');
+			if (iter != _f[_state].end()) {
+				_state = iter->second;
+				_curToken += ch;
+			} else {
+				_state = 9;
+			}
+		} else if(isOther(ch)) {
+			//其他字符，如果没有定义则报错
+			map<char, int>::iterator iter = _f[_state].find('o');
+			if (iter != _f[_state].end()) {
+				_state = iter->second;
+				if (_state != 9 && _state != 8) {
+					_curToken += ch;
+				}
+			} else {
+				_state = 9;
+			}
+		} else {
+			//不是数字，也不是其他字符，说明可能是.,'e','E',' ','+','-'
+			map<char, int>::iterator iter = _f[_state].find(ch);
+			if (iter != _f[_state].end()) {
+				_state = iter->second;
+				_curToken += ch;
+			} else {
+				_state = 9;
+			}
+		}
+	}
+
+	return res;
+}
+
+// @breif:      判断字符是否为数字
+bool FloatPoint::isDigit(char ch) {
+	bool is_digit = false;
+	if (ch >='0' && ch <='9') {
+		is_digit = true;
+	}
+	return is_digit;
+}
+
+// @breif:      输出错误信息
+int FloatPoint::error_log(std::string& mess){
+	int res = -1;
+	mess = "not find";
+	return res;
+}
+
+
+// @breif:      拿到当前已经识别出来的浮点数串
+int FloatPoint::getCurToken(std::string& token) {
+	int res = 0;
+	token = _curToken;
+	return res;
+}
+
+// @breif:      判断是否为数字、e/E、+/-、空格之外的字符
+bool FloatPoint::isOther(char ch) {
+	bool is_other = false;
+	//不是数字，也不在字符集合中，则表示是其他字符
+	if (!isDigit(ch)
+	    && all_char_set.find(ch) == all_char_set.end()) {
+		is_other = true;
+	}
+	return is_other;
+}
+// @breif:      获取下一个字符，先将字符指针加一，如果识别到了最后一个字符之后，则返回-1
+int FloatPoint::getChar(char* ch) {
+	int res=0;
+	int _input_len = _input_str.length();
+	++ptr;
+	if (ptr >= _input_len) {
+		res = -1;
+	} else {
+		*ch = _input_str[ptr];
+	}
+	return res;
+}
+
+
+// @breif:     恢复自动机状态
+int FloatPoint::restoreAutonmata() {
+	int res = 0;
+	//当前状态
+	_state = 0;
+	//当前识别的结果
+	_curToken = "";
+	return res;
+}
+
+int main() {
+	std::string _input_str = "12.3*********klj12.2e2jj778";
+	_input_str = "-23.3E+234asdfasdfasdf928,234.9;asdfwer7n3e3.";
+	FloatPoint _float_point(_input_str);
+	std::string token;
+	while (_float_point.getNextToken(token) != -1) {
+		cout << token << endl;
+		token = "";
+	}
+	//最后一个token有可能是走到了结尾
+	if (token!="") {
+		cout << token<<endl;
+	}
+	return 0;
+}
